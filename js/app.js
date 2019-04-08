92b18b405189bab4b40d3dc38ef015040da2798a
Ethan <N/A>
2019-04-08 11:12:10 -0700

    Add winner logic, adding new hand board reset

--- js/app.js
+++ js/app.js
@@ -46,6 +46,9 @@ class Player {
       deck.shift();
       this.showLastCard();
       this.calcTotal();
+      if (this.shown > 21) {
+        standing();
+      }
     }
   }
 }
@@ -103,7 +106,6 @@ class Comp extends Player {
     let cardImg = document.getElementById(`dealer${this.hand.length}`);
     cardImg.classList.remove("outline");
     cardImg.classList.add("back");
-    
   }
 
   showSecondCard() {
@@ -118,6 +120,33 @@ class Comp extends Player {
 
 var game = {
   dealAll() {
+    //resets board to neutral state
+    //clear hands
+    let playhand = player.hand.length;
+    let comphand = computer.hand.length;
+
+    for (let i = 0; i < playhand; i++) {
+      let str = `play${i + 1}`;
+      let cardImg = document.getElementById(str);
+      console.log(cardImg);
+      cardImg.classList.add("outline2");
+      console.log(player.hand[0].img);
+      cardImg.classList.remove(player.hand[0].img);
+      player.hand.shift();
+      console.log(player.hand);
+    }
+    for (let i = 0; i < comphand; i++) {
+      let str = `dealer${i + 1}`;
+      let cardImg = document.getElementById(str);
+      console.log(cardImg);
+      cardImg.classList.add("outline");
+      cardImg.classList.remove(computer.hand[0].img);
+      computer.hand.shift();
+    }
+    //reset text on screen
+    
+
+    initDeck();
     setTimeout(function() {
       player.addCard();
     }, 200);
@@ -129,6 +158,9 @@ var game = {
     }, 600);
     setTimeout(function() {
       computer.addCard();
+      if (player.shown == 21) {
+        standing();
+      }
     }, 800);
 
     // computer.addCard();
@@ -177,23 +209,77 @@ function initDeck() {
   shuffle();
 }
 
-var standing = ()=>{
-    
-    setTimeout(function() {
-        computer.showSecondCard();
-      }, 200);
+var standing = () => {
+  // document.getElementById("dealBut").disabled = false;
+  // document.getElementById("hitBut").disabled = true;
+  // document.getElementById("passBut").disabled = true;
+  setTimeout(function() {
+    computer.showSecondCard();
+  }, 200);
 
-      setTimeout(function() {
-   while(computer.shown<17&&computer.hand.length<5&&computer.shown<player.shown){
-        console.log(computer.shown);
-            computer.addCard();
-          
+  setTimeout(function() {
+    while (
+      computer.shown < 17 &&
+      computer.hand.length < 5 &&
+      computer.shown < player.shown
+    ) {
+      console.log(computer.shown);
+      computer.addCard();
     }
-}, 400);
-}
+  }, 400);
+  document.getElementById("dealBut").value = "New Hand";
+  document.getElementById("dealBut").disabled = false;
+  document.getElementById("hitBut").disabled = true;
+  document.getElementById("passBut").disabled = true;
+
+  setTimeout(function() {
+    findWinner();
+  }, 600);
+};
+
+var findWinner = () => {
+  console.log(document.getElementById("result").innerHTML);
+  if (player.shown <= 21 && computer.shown <= 21) {
+    if (player.shown == 21 && player.hand.length == 2) {
+      if (computer.shown != 21 && computer.hand.length > 2) {
+        document.getElementById("result").innerHTML = "YOU GOT A BLACKJACK!";
+        player.money +=
+          Number(document.getElementById("betAmount").innerText) * 1.5;
+      } else if (computer.shown == 21 && computer.hand.length == 2) {
+        document.getElementById("result").innerHTML =
+          "Both got blackjack, it's a tie!";
+      }
+    } else if (computer.shown > player.shown) {
+      document.getElementById("result").innerHTML = "The dealer's hand won";
+      player.money -= document.getElementById("betAmount").innerText;
+    } else if (player.shown > computer.shown) {
+      document.getElementById("result").innerHTML = "Your hand won!";
+      player.money += document.getElementById("betAmount").innerText;
+    } else {
+      document.getElementById("result").innerHTML = "It's a tie!";
+    }
+  } else if (player.shown > 21 && computer.shown <= 21) {
+    document.getElementById("result").innerHTML = "You bust! Dealer won";
+    player.money -= document.getElementById("betAmount").innerText;
+  } else if (player.shown <= 21 && computer.shown > 21) {
+    document.getElementById("result").innerHTML = "The dealer bust! You've won";
+    player.money += document.getElementById("betAmount").innerText;
+  } else {
+    alert("EDGE CASE PLEASE ADDRESS");
+  }
+  document.getElementById("nowMoney").innerText = player.money;
+};
+
+var betChange = num => {
+  let currentBet = Number(document.getElementById("betAmount").innerText);
+  if (currentBet + num > 0 && currentBet + num <= player.money) {
+    document.getElementById("betAmount").innerText = currentBet + num;
+  } else if (currentBet + num > player.money) {
+    document.getElementById("betAmount").innerText = player.money;
+  }
+};
 
 //working area
 
 let player = new Player();
 let computer = new Comp();
-initDeck();
